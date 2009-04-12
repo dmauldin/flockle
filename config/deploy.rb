478973cd10f3c63fced6575c32d6564a3152f975
David Mauldin <dmauldin@gmail.com>
2009-04-12 01:49:36 -0700

    Added capistrano files.

---
+++ config/deploy.rb
@@ -0,0 +1,37 @@
+default_run_options[:pty] = true
+set :scm, "git"
+set :user, "neura"
+set :application, "flockle"
+set :repository,  "git@github.com:dmauldin/flockle.git"
+set :domain, "flockle.com"
+
+ssh_options[:forward_agent] = true
+set :branch, "master"
+set :deploy_via, :remote_cache
+set :git_shallow_clone, 1
+set :git_enable_submodules, 1
+set :use_sudo, false
+set :deploy_to, "/home/#{user}/rails/#{application}"
+
+role :app, domain
+role :web, domain
+role :db,  domain, :primary => true
+
+namespace :deploy do
+  desc "Restarting mod_rails with restart.txt"
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+ 
+  [:start, :stop].each do |t|
+    desc "#{t} task is a no-op with mod_rails"
+    task t, :roles => :app do ; end
+  end
+end
+
+desc "Symlink host specific config files"
+task :after_update_code do
+  run "ln -s #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+  # run "ln -s #{shared_path}/config/initializers/site_keys.rb #{release_path}/config/initializers/site_keys.rb"
+  # run "ln -s #{shared_path}/config/initializers/hoptoad.rb #{release_path}/config/initializers/hoptoad.rb"
+end
